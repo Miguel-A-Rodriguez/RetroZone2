068f35ac1341c6296b70d5e28d11e80306ba9f5a
Miguel Rodriguez <ultralord239@gmail.com>
2021-08-05 01:25:52 +0900

    finished pricing page and footer need to refactor css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,19 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import { toast } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
+import styled from "styled-components";
 import Logo from "../images/Logo.png";
 
-
 toast.configure()
+
+const IconStylecolor =  styled.span`
+color: #3b5998;
+svg{
+    background-color: white;
+    border-radius: 50%;
+}
+
+ `;
 
 const Footer = () => {
 
@@ -22,10 +31,13 @@ const Footer = () => {
         <div> <Link to="/"><span><img src={Logo}alt="Retro Zone Image" /></span></Link> </div>
            <div className=""><span>Copyright 2021</span></div>
            <div className="social-media-links">
+               
            <span onClick={notify}> <FontAwesomeIcon icon={faEnvelope} /> </span>
-           <span> <FontAwesomeIcon icon={faFacebook} /> </span>
-           <span> <FontAwesomeIcon icon={faInstagramSquare} /> </span>
-           </div>
+
+           <IconStylecolor> <FontAwesomeIcon icon={faFacebook} /> </IconStylecolor>
+
+           <div style={{backgroundColor: "white", borderRadius: "10%", display: "flex", width:"12px", height: "14px", justifyContent: "center", alignItems: "center", marginTop: "2px"}}> <FontAwesomeIcon style={{color: "rgb(233, 89, 80)", fontSize: "100%"}} icon={faInstagramSquare} /> </div>
+        </div>
      </footer>
 )
 }
