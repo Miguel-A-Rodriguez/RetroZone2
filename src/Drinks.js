299a02008ae014e7344afc540fc47d32186943e8
Miguel Rodriguez <ultralord239@gmail.com>
2021-08-09 12:09:45 +0900

    finished 2 pages, added facebook page modal

--- src/Drinks.js
+++ src/Drinks.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from "./components/Footer";
 import Nav from "./components/Nav";
 
 const Drinks = () => {
@@ -6,58 +7,50 @@ const Drinks = () => {
         <body>
             <Nav/>
 
-            <section className="kv-container">
-            <span>Sip Sip Game</span>
-            <span><img src="" alt="drinks-kv-img" /></span>
+            <section className="drinks-kv-container">
+                <span>SIP SIP GAME
+                </span>
+                <div className="drinks-kv-img"></div>
             </section>
 
-            <section className="drinks-txt">
-                <span>Soft Drinks</span>
-                <p>Soda</p>
-                <p>Water</p>
-            </section>
+            <div class="drink-txt-box">
+                <section className="drinks-txt">
+                    <span>Soft Drinks</span>
+                    <p>Soda</p>
+                    <p>Water</p>
+                </section>
+                <section className="drinks-txt">
+                    <span>Craft Beer</span><br/>
+                    <span>$5.00</span>
+                    <p>While our selection does change, we<br/> only carry  Florida brews.</p>
+                </section>
+                <section className="drinks-txt">
+                    <span>Domestic</span><br/>
+                    <span>$3.00</span>
+                    <p>Bud Light</p>
+                    <p>Miller Lite</p>
+                </section>
+            
+                <section className="drinks-txt">
+                    <span>Import/Seltzer</span><br/>
+                    <span>$4.00</span>
+                    <p>Heineken</p>
+                    <p>White Claw</p>
+                </section>
+                <section className="drinks-txt">
+                    <span>Wine</span><br/>
+                    <span>$5.00</span>
+                    <p>Selection varies by week.</p>
+                </section>
+                <section className="drinks-txt">
+                    <span>Snacks</span><br/>
+                    <span>$1.00</span>
+                    <p>Chips</p>
+                </section>
+            
+            </div>
 
-            <section className="drinks-txt">
-                <span>Craft Beer</span>
-                <span>$5.00</span>
-                <p>While our selection does change, we only carry Florida brews.</p>
-            </section>
-
-            <section className="drinks-txt">
-                <span>Domestic</span>
-                <span>$3.00</span>
-                <p>Bud Light</p>
-                <p>Miller Lite</p>
-            </section>
-
-            <section className="drinks-txt">
-                <span>Import/Seltzer</span>
-                <span>$4.00</span>
-                <p>Heineken</p>
-                <p>White Claw</p>
-            </section>
-
-            <section className="drinks-txt">
-                <span>Wine</span>
-                <span>$5.00</span>
-                <p>Selection varies by week.</p>
-            </section>
-
-            <section className="drinks-txt">
-                <span>Snacks</span>
-                <span>$1.00</span>
-                <p>Chips</p>
-            </section>
-
-            <footer>
-                <div className=""><img src="" alt="Retro Zone Img" /></div>
-                <div className=""><span>Copyright 2021</span></div>
-                <div className="social-media-links">
-                <i class="fas mail"></i>
-                <i class="facebook"></i>
-                <i class="instagram"></i>
-                </div>
-            </footer>
+            <Footer/>
         </body>
     )
 }
--- src/Events.js
+++ src/Events.js
@@ -1,59 +1,73 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Footer from "./components/Footer";
 import Nav from "./components/Nav";
-
 const Events = () => {
+    useEffect(() => {
+        const scriptTag = document.createElement('script');
+
+        scriptTag.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v11.0";
+        scriptTag.async = true;
+
+        document.body.appendChild(scriptTag);
+        return () => {
+            document.body.removeChild(scriptTag);
+        }
+    }, []);
+
+
     return (
         <body>
             <Nav/>
+            <div id="fb-root"></div>
+            <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v11.0" nonce="dYQQZCmY">
 
-            <section className="kv-container">
-            <span>FUN FOR THE</span>
-            <span>WHOLE FAMILY</span>
-            <span><img src="" alt="drinks-kv-img" /></span>
+            </script>
+            <section className="events-kv-container">
+                <span>FUN FOR THE WHOLE FAMILY
+                </span>
+                <div className="events-kv-img"></div>
             </section>
 
-            <section className="drinks-txt">
-                <span>Big time 80's</span>
-                <span>SPRING</span>
-                <span>BREAK-A-THON</span>
-                <p>Extended Hours</p>
-                <p>(Open at 12 during the week)</p>
-                <p>2/$30 Game Play</p>
-            </section>
-
-            <section className="drinks-txt">
-                <span>GAME & GRUB</span>
-                <span>3/27/21</span>
-                <p>Kona Ice of Fort Myers Shores will</p>
-                 <p>join us from 5-8pm. Get a sweet </p>
-                 <p>treat and earn $5 off at the</p>
-                 <p>arcade!</p>
-                   </section>
-
-            <section className="drinks-txt">
-                <span>GAME & GRUB</span>
-                <span>4/24/21</span>
-                <p>Dynamite Street Eats will join us from </p>
-                <p>5-8pm. Get some grub and earn $5 </p>
-                <p>off at the arcade!</p>
-            </section>
-
-            <section className="drinks-txt">
-                <span>POKEMON/LEAGUE</span>
-                <p>Every Wednesday @ 6:30pm</p>
-                <p>Registration: 5:30pm-6:25pm</p>
-                <p>Entry: $5.00</p>
-                <p>Format: Standard</p>
-                <p>Prizing: Pack/Arcade credit based on attendance.</p>
-            </section>
-
-            <section className="drinks-txt">
-                <span>Yu-Gi-Oh Weekly</span>
-                <p>Every Friday at 7:00pm</p>
-                <p>Entry: $5.00</p>
-                <p>Format: Advanced (Swiss Rounds with payout after final round of Swiss)</p>
-                <p>Prizing: Pack/Arcade credit based on attendance.</p>
-            </section>
+            <div class="events-txt-box">
+                <section className="events-txt">
+                    <span>Big time 80's</span>
+                    <span>SPRING</span>
+                    <span>BREAK-A-THON</span>
+                    <p>Extended Hours</p>
+                    <p>(Open at 12 during the week)</p>
+                    <p>2/$30 Game Play</p>
+                </section>
+                <section className="events-txt">
+                    <span>GAME & GRUB</span>
+                    <span>3/27/21</span>
+                    <p>Kona Ice of Fort Myers Shores will</p>
+                     <p>join us from 5-8pm. Get a sweet </p>
+                     <p>treat and earn $5 off at the</p>
+                     <p>arcade!</p>
+                       </section>
+                <section className="events-txt">
+                    <span>GAME & GRUB</span>
+                    <span>4/24/21</span>
+                    <p>Dynamite Street Eats will join us from </p>
+                    <p>5-8pm. Get some grub and earn $5 </p>
+                    <p>off at the arcade!</p>
+                </section>
+                <section className="events-txt">
+                    <span>POKEMON/LEAGUE</span>
+                    <p>Every Wednesday @ 6:30pm</p>
+                    <p>Registration: 5:30pm-6:25pm</p>
+                    <p>Entry: $5.00</p>
+                    <p>Format: Standard</p>
+                    <p>Prizing: Pack/Arcade credit based on attendance.</p>
+                </section>
+                <section className="events-txt">
+                    <span>Yu-Gi-Oh Weekly</span>
+                    <p>Every Friday at 7:00pm</p>
+                    <p>Entry: $5.00</p>
+                    <p>Format: Advanced (Swiss Rounds with payout after final round of Swiss)</p>
+                    <p>Prizing: Pack/Arcade credit based on attendance.</p>
+                </section>
+            </div>
 
             <section className="bottom-event-txt">
                 <h1>WHATS HAPPENING AT THE RETRO ZONE</h1>
@@ -63,18 +77,13 @@ const Events = () => {
             </section>
 
             <section className="facebook-conent-container">
-                
+            {/* <div class="fb-page" 
+                    data-tabs="timeline"
+                    data-href="https://www.facebook.com/retrozonearcade/">
+            </div> */}
+            <div class="fb-page" data-href="https://www.facebook.com/retrozonearcade/" data-tabs="timeline" data-width="500" data-height="500" data-small-header="false" data-adapt-container-width="false" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote></div>
             </section>
-
-            <footer>
-                <div className=""><img src="" alt="Retro Zone Img" /></div>
-                <div className=""><span>Copyright 2021</span></div>
-                <div className="social-media-links">
-                <i class="fas mail"></i>
-                <i class="facebook"></i>
-                <i class="instagram"></i>
-                </div>
-            </footer>
+            <Footer/>
         </body>
     )
 }
